63cdfdaa92a16e80a25957b358f97ed0ca6826a4
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-29 16:39:46 -0400

    have everything working now I need to style it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class ImageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      caption: '',
+    }
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleInput = this.handleInput.bind(this);
+  }
+  handleInput(event){
+    console.log(event.target.value);
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.addImage(this.state);
+  }
+  render() {
+    return(
+      <form className ="col" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="url">Image Url</label>
+          <input type="url" className="form-control" id="url" name="url" value={this.state.url} onChange={this.handleInput}/>
+          <label htmlFor="caption">Image Caption</label>
+          <input type="text" className="form-control" id="caption" name="caption" value={this.state.caption} onChange={this.handleInput}/>
+        </div>
+        <button className="btn btn-primary" type="submit">Add</button>
+    </form>
+    )
+  }
+}
+class ImageList extends Component {
+  render() {
+    const images = this.props.images.map((image, index) => <li className="list-style"key={index}><img className="img-style"src={image.url} alt=""/><p>{image.caption}</p></li>)
+    return(
+    <div className="container">
+    <ul>
+    {images}
+    </ul>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+  class ImageBoard extends Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        images: [{}],
+      }
+      this.addImage = this.addImage.bind(this);
+    }
+    addImage(image){
+      console.log('image', image);
+      const images = this.state.images
+      images.push(image);
+      this.setState({images});
+    }
+  render() {
+    return(
+      <div>
+        <ImageForm addImage={this.addImage}/>
+        <ImageList images={this.state.images}/>
+      </div>
+    )
+  }
+}
+
+export default ImageBoard;
